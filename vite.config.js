dcd0729696d847a580bad46e930d8732e1ba1581
Toney <ta.winston@outlook.com>
2025-05-13 18:39:33 -0400

    fixed bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "https://github.com/Gazdalman/react-final-project.git",
+    base: "/react-final-project",
   plugins: [react()],
 })
